db7198c550ecf155b1154c1bf0c1f135c97b9f5b
akshata <akshatamandre@gmail.com>
2022-05-26 23:30:56 -0700

    comment after master

--- GitPushTest/src/Class1.java
+++ GitPushTest/src/Class1.java
@@ -5,6 +5,7 @@ public class Class1 {
 		System.out.println("Hi Akshata!");
 		System.out.println("This is my comment");
 		System.out.println("This is my comment from github");
+		System.out.println("this is comment after pull");
 
 	}
 
